67ccdb0473f23a9e173ef3013bfc78a2daf00af7
Benjamin Sepulveda <benjamin0sepulveda4@gmail.com>
2024-08-12 16:04:25 -0400

    Se agregaron formularios login y register

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 
